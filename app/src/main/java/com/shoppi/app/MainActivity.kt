33ea05db9834aec3585b88151813b9b2672f5dbe
Yohan <gksdygks359@naver.com>
2023-02-11 17:39:09 +0900

    [섹션6] 하단 탭 구성

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -3,13 +3,24 @@ package com.shoppi.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-        Log.d("MyMain","onCreate!")
-        //fragment_product_detail
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList=null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
 
 
     }
